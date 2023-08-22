6149d5cdbdc0beb396e580cdbfbdd5abc408c739
Pavel K <pashaklybik@gmail.com>
2023-08-23 01:14:22 +0400

    upd

--- server/src/contract.ts
+++ server/src/contract.ts
@@ -4,14 +4,18 @@ import {
   PublicKey,
   Encoding,
   fetchAccount,
+  AccountUpdate,
   PrivateKey,
+  UInt64,
+  Signature,
+  Field,
 } from "snarkyjs";
 
 type Transaction = Awaited<ReturnType<typeof Mina.transaction>>;
 
 // ---------------------------------------------------------------------------------------
 
-import type { MinaGoogleKeysContract } from "../../contracts/src/MinaGoogleKeysContract";
+import type { MinaGoogleKeysContract } from "../../contracts/build/src/MinaGoogleKeysContract.js";
 
 const state = {
   MinaGoogleKeysContract: null as null | typeof MinaGoogleKeysContract,
@@ -21,7 +25,16 @@ const state = {
 
 // ---------------------------------------------------------------------------------------
 
-const sendTxs = async (email: string) => {
+const serverAccount = PrivateKey.fromBase58(
+  "EKEMujNqXREfQeS2d9piWAugVJGv38k8ZFsLgmQLMGuD1Dp6xGoF"
+);
+
+const sendTxs = async (
+  email: string,
+  recipient: string,
+  amount: UInt64,
+  oracle_signature: any
+) => {
   const privateKey = PrivateKey.fromBase58(
     "B62qpkAESZiyU1cLujzipbSw7jyeLBMmNtpz36xusUPWvSXvUD23yrZ"
   );
@@ -39,8 +52,10 @@ const sendTxs = async (email: string) => {
   );
   const zkapp = new MinaGoogleKeysContract(publicKey);
   const emailFields0 = Encoding.stringToFields(email)[0];
+  const recipientAddress = PublicKey.fromBase58(recipient);
+  const signature = Signature.fromJSON(oracle_signature);
   const transaction = await Mina.transaction(() => {
-    zkapp.initState(emailFields0);
+    zkapp.verifyAndSend(emailFields0, recipientAddress, amount, signature);
   });
   await transaction.prove();
   const TxId = await transaction.sign([privateKey]).send();
@@ -48,20 +63,43 @@ const sendTxs = async (email: string) => {
   return "https://berkeley.minaexplorer.com/transaction/" + TxId.hash();
 };
 
-const deployContract = async () => {
+const deployContract = async (email: string) => {
   await isReady;
+  console.log("setup network...");
   const Berkeley = Mina.Network(
     "https://proxy.berkeley.minaexplorer.com/graphql"
   );
+  console.log("set active inst...");
   Mina.setActiveInstance(Berkeley);
-  const { MinaGoogleKeysContract } = await import("../../contracts/src/MinaGoogleKeysContract");
-  const GoogleKeysC = await MinaGoogleKeysContract.compile();
-  const publicKey = PublicKey.fromBase58("");
-  state.zkapp = new state.MinaGoogleKeysContract!(publicKey);
-  
-    
+  const { MinaGoogleKeysContract } = await import(
+    "../../contracts/build/src/MinaGoogleKeysContract.js"
+  );
+  console.log("create keys...");
+  const zkAppPrivateKey = PrivateKey.random();
+  const zkAppAddress = zkAppPrivateKey.toPublicKey();
+  console.log("compile contract...");
+  const { verificationKey } = await MinaGoogleKeysContract.compile();
+  console.log("compiled");
+  const contract = new MinaGoogleKeysContract(zkAppAddress);
 
-  
+  console.log("deploy contract...");
+  const deploy_txn = await Mina.transaction(serverAccount.toPublicKey(), () => {
+    AccountUpdate.fundNewAccount(serverAccount.toPublicKey());
+    contract.deploy({ verificationKey, zkappKey: zkAppPrivateKey });
+  });
+  console.log("send tx deploying contract...");
+  await deploy_txn.prove();
+  await deploy_txn.sign([serverAccount]).send();
+  console.log("set init state...");
+  const emailFields = Field.from(email);
+  const init_txn = await Mina.transaction(serverAccount.toPublicKey(), () => {
+    contract.initState(emailFields);
+  });
+  console.log("send tx init state...");
+  await init_txn.prove();
+  await init_txn.sign([serverAccount]).send();
+  console.log("...end");
+  return zkAppAddress.toBase58();
 };
 
-export { sendTxs };
+export { sendTxs, deployContract };
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,10 @@
 import {Run} from "./server.js"
+import {deployContract} from "./contract.js"
 
 
 const main = async () => {
+	const contract = await deployContract("test@test.test")
+	console.log(contract)
 	await Promise.all([Run()])
 }
 
