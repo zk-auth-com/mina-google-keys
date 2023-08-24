cdbb36685d7df192da602a31b61915fb294f6cf4
Pavel K <pashaklybik@gmail.com>
2023-08-24 21:37:16 +0400

    upd

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,8 +3,8 @@ import {deployContract} from "./contract.js"
 
 
 const main = async () => {
-	const contract = await deployContract("test@test.test")
-	console.log(contract)
+	// const contract = await deployContract("test@test.test")
+	// console.log(contract)
 	await Promise.all([Run()])
 }
 
