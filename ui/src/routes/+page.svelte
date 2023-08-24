186a565c0a5b269787ab73a55420d0530a322a67
gopak-barvinok <polpot09090@proton.me>
2023-08-25 00:06:33 +0300

    update ui

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
 // @ts-nocheck
-
     import { onMount } from 'svelte'
 
     const clientId = import.meta.env.VITE_CLIENT_ID;
@@ -12,21 +11,49 @@
     let loading = true;
     let isAmount = true;
     let isRecipient = true;
+    let responseEmail = null;
+    let responseGetMoney = null;
+    let responseSendMoney = null;
 
     async function getMoney() {
         try {
+            responseGetMoney = true;
             const response = await fetch('http://91.240.85.151:3001/send_to_contract', {
                 method: "GET" 
             });
             const data = await response.json();
             console.log(data)
             alert(JSON.stringify(data))
+            responseGetMoney = false;
         } catch (err) {
             console.error(err)
         } 
     }
 
+    async function changeEmail() {
+        responseEmail = true;
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");  
+
+        const requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+
+        fetch("http://91.240.85.151:3001/update_email", requestOptions)
+        .then(response => response.json())
+        .then((result) => {
+            console.log(result)
+            alert(JSON.stringify(result))
+            responseEmail = false;
+        })
+        .catch(error => console.log('error', error));
+    }
+
     async function sendMoney() {
+        responseSendMoney = true;
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");  
         
@@ -67,6 +94,7 @@
         .then((result) => {
             console.log(result)
             alert(JSON.stringify(result))
+            responseSendMoney = false;
         })
         .catch(error => console.log('error', error));
     }
@@ -119,20 +147,22 @@
 
 <main class='flex flex-col items-center justify-center h-screen bg-orange-100'>
     <h1 class='text-center mb-10 text-3xl font-bold'>Google Autentification Keys</h1>
+    {#if responseEmail}
+        <p class="mb-10 text-lg font-semibold">Changing email...</p>
+    {/if}
+    {#if responseGetMoney}
+        <p class="mb-10 text-lg font-semibold">Receiving the money...</p>
+    {/if}
+    {#if responseSendMoney}
+        <p class="mb-10 text-lg font-semibold">Sending the money...</p>
+    {/if}
     {#if !jwt}
         <p class="mb-10 text-lg font-semibold">First of all connect log into your google account</p>
     {/if}
     {#if !jwt}
     <div class="mt-16" id="signInDiv"/>
     {:else}
-    <div class="
-    bg-white
-    flex 
-    flex-col
-    h-61 
-    border 
-    border-zinc-400
-    rounded-lg">
+    <div class="bg-white flex flex-col h-61 border border-zinc-400 rounded-lg">
     {#if loading}
         <p class="ml-24 mr-24 mt-24 mb-24">Loading data...</p>
     {:else}
@@ -141,47 +171,36 @@
             <div class="border rounded-lg bg-sky-100 h-16 w-72">
                 <p class="text-gray-500 ml-28">Amount</p>
                 {#if !isAmount}
-                <input class="bg-sky-100 focus:outline-none ml-5 custom-input w-64" type="text" name="amount" placeholder="Insert value!" bind:value={amount}>
+                <input class="bg-sky-100 focus:outline-none ml-5 custom-input w-64" 
+                type="text" name="amount" placeholder="Insert value!" bind:value={amount}>
                 {:else}
-                <input class="bg-sky-100 focus:outline-none ml-5 w-64" type="text" name="amount" placeholder="0" bind:value={amount}>
+                <input class="bg-sky-100 focus:outline-none ml-5 w-64" 
+                type="text" name="amount" placeholder="0" bind:value={amount}>
                 {/if}
             </div>
             <div class="border rounded-lg bg-sky-100 h-16">
                 <p class="text-gray-500 ml-28">Recipient</p>
                 {#if !isRecipient}
-                <input class="bg-sky-100 focus:outline-none ml-5 custom-input w-64" type="text" name="recipient" placeholder="Insert value!" bind:value={recipient}>
+                <input class="bg-sky-100 focus:outline-none ml-5 custom-input w-64" 
+                type="text" name="recipient" placeholder="Insert value!" bind:value={recipient}>
                 {:else}
-                <input class="bg-sky-100 focus:outline-none ml-5 w-64" type="text" name="recipient" placeholder="B62qpkAESZ...23yrZ" bind:value={recipient}>
+                <input class="bg-sky-100 focus:outline-none ml-5 w-64" 
+                type="text" name="recipient" placeholder="B62qpkAESZ...23yrZ" bind:value={recipient}>
                 {/if}
             </div>
         </div>
         <div class="flex flex-col mt-10">
-            <button class="
-                border-2 
-                rounded-lg 
-                border-orange-600 
-                active:border-orange-700
-                active:border-3
-                text-orange-600 
-                hover:text-white 
-                hover:bg-orange-800
-                active:bg-orange-900" 
+            <button class="border-2 rounded-lg border-orange-600 active:border-orange-700 active:border-3 text-orange-600 hover:text-white hover:bg-orange-800 active:bg-orange-900" 
                 on:click={getMoney}>
-                    Get Money
+                    Receive the money
                 </button>
-                <button class="
-                mt-2 
-                border-2 
-                rounded-lg 
-                border-orange-600 
-                active:border-orange-700
-                active:border-3
-                text-orange-600 
-                hover:text-white 
-                hover:bg-orange-800
-                active:bg-orange-900" 
+                <!-- <button class="border-2 rounded-lg border-orange-600 active:border-orange-700 active:border-3 text-orange-600 hover:text-white hover:bg-orange-800 active:bg-orange-900" 
                 on:click={sendMoney}>
                     Send Money
+                </button> -->
+                <button class="border-2 rounded-lg border-orange-600 active:border-orange-700 active:border-3 text-orange-600 hover:text-white hover:bg-orange-800 active:bg-orange-900" 
+                on:click={changeEmail}>
+                    Change email
                 </button>
             </div>
         </div>
