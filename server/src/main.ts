ae1a4fc190db7f81b0f5227d0e8afe5454912618
Pavel K <pashaklybik@gmail.com>
2023-09-08 04:41:58 +0400

    upd

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,17 +8,17 @@ interface signature {
 
 const main = async () => {
   // const tx = await updateEmail("pashaklybik@gmail.com")
-  const sign = {
-    r: "3363833629330128233651788224576693951330222458872860775675028016856115168133",
-    s: "25690860517388675946198652643138055228491779636793399414315724874831248703777",
-  } as signature;
-  const tx = await sendTxs(
-    "pashaklybik@gmail.com",
-    "B62qo2mibtGCPUWTfHbaLFaQo4po1RQoAyC8kfgNNYTsNxHe173FSN2",
-    1000000000,
-    sign
-  );
-  console.log(tx);
+  // const sign = {
+  //   r: "3363833629330128233651788224576693951330222458872860775675028016856115168133",
+  //   s: "25690860517388675946198652643138055228491779636793399414315724874831248703777",
+  // } as signature;
+  // const tx = await sendTxs(
+  //   "pashaklybik@gmail.com",
+  //   "B62qo2mibtGCPUWTfHbaLFaQo4po1RQoAyC8kfgNNYTsNxHe173FSN2",
+  //   1000000000,
+  //   sign
+  // );
+  // console.log(tx);
   await Promise.all([Run()]);
 };
 
